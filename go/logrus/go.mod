3b336cbe7358065ad926b6ae0d14ef187d4bc909
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2020-07-21 17:57:09 +0900

    go (sirupsen/logrus): Add sample of logrus

---
+++ go/logrus/go.mod
@@ -0,0 +1,5 @@
+module github.com/ymotongpoo/cloud-logging-configurations/go/logrus
+
+go 1.14
+
+require github.com/sirupsen/logrus v1.6.0
---
+++ go/logrus/main.go
@@ -0,0 +1,43 @@
+// Copyright 2020 Yoshi Yamaguchi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+var log *logrus.Logger
+
+func init() {
+	log = logrus.New()
+	log.Formatter = &logrus.JSONFormatter{
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "timestamp",
+			logrus.FieldKeyLevel: "severity",
+			logrus.FieldKeyMsg:   "message",
+		},
+		TimestampFormat: time.RFC3339Nano,
+	}
+	log.Out = os.Stdout
+}
+
+func main() {
+	log.Info("This is info level log.")
+	log.Warn("This is waning level log.")
+	log.Error("This is error level log.")
+}
