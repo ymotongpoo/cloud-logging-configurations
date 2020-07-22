c714d2380c8f6c0f261758b98924222e463f2173
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2020-07-22 11:08:17 +0900

    python (python-json-logger): Add python-json-logger sample

---
+++ python/jsonlogger/logger.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#
+# Copyright 2020 Yoshi Yamaguchi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import datetime
+import logging
+import sys
+
+from pythonjsonlogger import jsonlogger
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get('timestamp'):
+            log_record['timestamp'] = datetime.fromtimestamp(record.created).isoformat()
+        if log_record.get('severity'):
+            log_record['severity'] = log_record['severity'].upper()
+        else:
+            log_record['severity'] = record.levelname
+
+def getJSONLogger(name):
+    logger = logging.getLogger(name)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = CustomJsonFormatter('%(timestamp)s %(severity)s %(name)s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    return logger
+
+if __name__ == '__main__':
+    logger = getJSONLogger("python-json-log-sample")
+    logger.info("This is info level log.")
+    logger.warning("This is warning level log.")
+    logger.error("This is error level log.")
