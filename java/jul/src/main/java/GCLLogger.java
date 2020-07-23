30b59d2037bbf6fd5427ecf61d11793ca4672f82
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2020-07-23 22:18:05 +0900

    java (jul): [WIP] Add sample of java.util.logging

---
+++ java/jul/src/main/java/GCLLogger.java
@@ -0,0 +1,31 @@
+// Copyright 2020 Yoshi Yamaguchi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sample.jul;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.LogManager;
+
+public class GCLLogger {
+    public static void main(final String[] args) throws IOException {
+        LogManager.getLogManager().readConfiguration(
+            GCLLogger.class.getResourceAsStream("logging.properties"));
+        Logger logger = Logger.getLogger("jul-sample");
+
+        logger.info("This is info level log.");
+        logger.warning("This is warning level log.");
+        logger.severe("This is error level log.");
+    }
+}
